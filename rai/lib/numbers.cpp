367d4609c7c8f8faaa38857bd1888794584dcfcd
Gabriel Mendanha <gabrielmendanha@hotmail.com>
2019-01-22 02:59:12 -0300

    fix numero

--- rai/lib/numbers.cpp
+++ rai/lib/numbers.cpp
@@ -81,7 +81,7 @@ bool rai::uint256_union::decode_account (std::string const & source_a)
 		{
 			if (ibanks_prefix)
 			{
-				auto i (source_a.begin () + (ibanks_prefix ? 7 : 5));
+				auto i (source_a.begin () + (ibanks_prefix ? 4 : 5));
 				if (*i == '1' || *i == '3')
 				{
 					rai::uint512_t number_l;
